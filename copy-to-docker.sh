5288a06c2dc84e6474a00dec1c5ea36db0ab3f6e
hiro <hirohiro716@gmail.com>
2024-09-03 16:44:09 +0900

    Fix script

--- copy-to-docker.sh
+++ copy-to-docker.sh
@@ -5,6 +5,6 @@ if [ ! -d $dockerDirectory ]; then
     echo "Cannot find docker directory."
     exit 1
 fi
-rsync -r -u $currentDirectory/www/ $dockerDirectory/ --exclude 'vendor' --exclude 'vendor/' --exclude 'database/'
+rsync -r -u $currentDirectory/www/ $dockerDirectory/ --exclude 'vendor' --exclude 'vendor/' --exclude 'database.db'
 find $dockerDirectory/ -type f -print0 | xargs -0 chmod 666 2>/dev/null
 find $dockerDirectory/ -type d -print0 | xargs -0 chmod 777 2>/dev/null
