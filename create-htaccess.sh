c936acd3749aa060946fe8dc26bfe87f00688a53
hiro <hirohiro716@gmail.com>
2024-09-03 11:48:47 +0900

    Change the placement of htaccess

---
+++ create-htaccess.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd `dirname $0`
+content="<Files ~ \"*\">
+deny from all
+< /Files>"
+echo "$content" >.htaccess
+echo "$content" >www/class/.htaccess
+echo "$content" >www/template/.htaccess
+echo "$content" >www/database/.htaccess
+echo "$content" >www/vendor/.htaccess
+find ./ -type f -name ".htaccess" | xargs chmod o-rxw
