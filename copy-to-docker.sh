63261a082babf6acd6ac2f217ae725130a38e9e2
hiro <hirohiro716@gmail.com>
2024-11-26 22:27:55 +0900

    Change docker configuration

--- copy-to-docker.sh
+++ copy-to-docker.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 currentDirectory=`pwd`
-dockerDirectory=$currentDirectory/../../docker/my-bookmarks
+dockerDirectory=$currentDirectory/docker
 if [ ! -d $dockerDirectory ]; then
     echo "Cannot find docker directory."
     exit 1
 fi
 rsync -r -u $currentDirectory/www/ $dockerDirectory/ --exclude 'vendor' --exclude 'vendor/' --exclude 'database.db'
-find $dockerDirectory/ -type f -print0 | xargs -0 chmod 666 2>/dev/null
-find $dockerDirectory/ -type d -print0 | xargs -0 chmod 777 2>/dev/null
+if [ ! -e $dockerDirectory/database/database.db ]; then
+    cp $currentDirectory/www/database/database.db $dockerDirectory/database/database.db
+fi
+if [ ! -d $dockerDirectory/template/compile ]; then
+    mkdir $dockerDirectory/template/compile
+fi
+find $dockerDirectory/ -type f -print0 | xargs -0 chmod 660
+find $dockerDirectory/ -type d -print0 | xargs -0 chmod 770
+chown -R www-data:www-data $dockerDirectory/*
--- create-htaccess.sh
+++ create-htaccess.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd `dirname $0`
 content="deny from all"
-echo "$content" >.htaccess
-echo "$content" >www/class/.htaccess
-echo "$content" >www/template/.htaccess
-echo "$content" >www/database/.htaccess
-echo "$content" >www/vendor/.htaccess
-find ./ -type f -name ".htaccess" | xargs chmod o-rxw
+echo "$content" >docker/class/.htaccess
+echo "$content" >docker/template/.htaccess
+echo "$content" >docker/database/.htaccess
+echo "$content" >docker/vendor/.htaccess
+find ./ -type f -name ".htaccess" | xargs chmod 660
+find ./ -type f -name ".htaccess" | xargs chown www-data:www-data
