874fa8cf52eb312d5a99a66363ff5bc51da7dd18
hiro <hirohiro716@gmail.com>
2024-11-27 19:38:37 +0900

    Change to not require SSL

--- www/class/AbstractWebPage.php
+++ www/class/AbstractWebPage.php
@@ -11,8 +11,7 @@ use hirohiro716\Scent\Helper;
 abstract class AbstractWebPage extends Scent\Smarty\AbstractWebPage
 {
     
-    public const REQUIRE_SECURE_CONNECTION = true;
-    // public const REQUIRE_SECURE_CONNECTION = false;
+    public const REQUIRE_SECURE_CONNECTION = false;
     
     public function __construct()
     {
